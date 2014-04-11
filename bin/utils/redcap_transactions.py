e17659b75ff0222cc2b03319ae86bc14a8c44f3f
Mohan Das Katragadda <mohan88@ufl.edu>
2014-04-11 14:16:41 -0400

    Remove unnecessary print statements

--- bin/utils/redcap_transactions.py
+++ bin/utils/redcap_transactions.py
@@ -41,7 +41,6 @@ class redcap_transactions:
         source_data = etree.parse(source_data_schema_file)
 
         fields = ','.join(field.text for field in source_data.iter('field'))
-        print fields
         
         if redcap_uri is None:
             host = '127.0.0.1:8998'
