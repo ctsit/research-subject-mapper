d226566e3d3161a5b27d276c7f430c3db467ea22
Philip Chase <pbc@ufl.edu>
2014-05-12 15:19:13 -0400

    Update setup.py to reflect release

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Research Subject Mapper',
-    version='0.1.0',
+    version='0.7.0',
     author='Christopher P Barnes, Philip Chase, Nicolas Rejack',
     author_email='cpb@ufl.edu, pbc@ufl.edu, nrejack@ufl.edu',
-    packages=['bin/'],
-    scripts=['bin/generate_subject_map_input.py'],
+    packages=['bin'],
+    scripts=['bin/generate_subject_map_input.py', 'bin/generate_subject_map.py'],
     url='',
-    license='LICENSE',
-    description='Subject map input generator',
+    license='BSD 3-Clause',
+    description='A suite of tools to curate and manage the people identifiers in a multi-site clinical research project.',
     long_description=open('README.md').read(),
     install_requires=[
         "requests >= 2.2.1",
         "lxml >= 3.2.4",
         "pysftp >= 0.2.2",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
     ],
 )
